977fc5f524b1b9a3b5ddd742a0db66857d9805d2
강민석 <bluekms@shiftup.co.kr>
2026-04-29 17:49:53 +0900

    FK 컬럼이 대상 TableSet에 존재하는지 빌드 시점에 검증

--- SchemaInfoScanner/Resources/Messages.Designer.cs
+++ SchemaInfoScanner/Resources/Messages.Designer.cs
@@ -300,6 +300,18 @@ internal static class Messages
     internal static string InvalidFullName
         => ResourceManager.GetString("InvalidFullName", Culture)!;
 
+    internal static string ForeignKeyValidationFailed
+        => ResourceManager.GetString("ForeignKeyValidationFailed", Culture)!;
+
+    internal static string ForeignKeyColumnNotFound
+        => ResourceManager.GetString("ForeignKeyColumnNotFound", Culture)!;
+
+    internal static string SwitchForeignKeyConditionColumnNotFound
+        => ResourceManager.GetString("SwitchForeignKeyConditionColumnNotFound", Culture)!;
+
+    internal static string SwitchForeignKeyColumnNotFound
+        => ResourceManager.GetString("SwitchForeignKeyColumnNotFound", Culture)!;
+
     internal static class Composite
     {
         internal static CompositeFormat AlreadyVisited => CompositeFormat.Parse(Messages.AlreadyVisited);
@@ -382,5 +394,8 @@ internal static class Messages
         internal static CompositeFormat FactoryDictionaryKeyMustBePrimitiveType => CompositeFormat.Parse(Messages.FactoryDictionaryKeyMustBePrimitiveType);
         internal static CompositeFormat FactoryNotSupportedRecordType => CompositeFormat.Parse(Messages.FactoryNotSupportedRecordType);
         internal static CompositeFormat IsRecordHashSetType => CompositeFormat.Parse(Messages.IsRecordHashSetType);
+        internal static CompositeFormat ForeignKeyColumnNotFound => CompositeFormat.Parse(Messages.ForeignKeyColumnNotFound);
+        internal static CompositeFormat SwitchForeignKeyConditionColumnNotFound => CompositeFormat.Parse(Messages.SwitchForeignKeyConditionColumnNotFound);
+        internal static CompositeFormat SwitchForeignKeyColumnNotFound => CompositeFormat.Parse(Messages.SwitchForeignKeyColumnNotFound);
     }
 }
